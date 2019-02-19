618bd3d82782df15a9f79d50abc2c7790435453b
brentroberts989 <prog@chaos.local>
2019-02-18 22:03:34 -0500

    MORE SPEED

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -36,8 +36,8 @@ public class Elevator {
 		//The location of the magnetic switch in inches, just below trigger point
 		MAG_SWITCH_POINT = 23.65, //was 23.75 
 		//The maximum power that the elevator can be run at upward
-		MAX_UP_POWER = .45, // was 1
-		MAX_DOWN_POWER = -0.25, // was -0.8 
+		MAX_UP_POWER = .55, // was 1
+		MAX_DOWN_POWER = -0.3, // was -0.8 
 		//The minimum power that the elevator can be run
 		MIN_UP_POWER = 0.11,
 		MIN_DOWN_POWER = -0.08,
